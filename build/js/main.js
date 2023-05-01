3bd4d902b19b13831e3e6a931416b3cf48d8c788
Eduardo O <viverlin7@gmail.com>
2023-05-01 14:38:15 -0600

    functions, never, enums, typealiases, literal types, function types, function interface, optional parameters, default parameters, never, rest parameter, cutom typeguards

--- build/js/main.js
+++ build/js/main.js
@@ -1,10 +1,89 @@
 "use strict";
-// Enum
-var Grades;
-(function (Grades) {
-    Grades[Grades["a"] = 0] = "a";
-    Grades[Grades["b"] = 1] = "b";
-    Grades[Grades["c"] = 2] = "c";
-    Grades[Grades["d"] = 3] = "d";
-    Grades[Grades["e"] = 4] = "e";
-})(Grades || (Grades = {}));
+/* Enums */
+/* unlike most typescript features, Enums are not a type-level addition to Javascript but something added to the language and the run time */
+var Grade;
+(function (Grade) {
+    Grade[Grade["u"] = 1] = "u";
+    Grade[Grade["d"] = 2] = "d";
+    Grade[Grade["c"] = 3] = "c";
+    Grade[Grade["b"] = 4] = "b";
+    Grade[Grade["a"] = 5] = "a";
+})(Grade || (Grade = {}));
+console.log(Grade.a);
+let vanHalen = {
+    active: false,
+    name: "eddie",
+    albums: [1, "hi"],
+};
+// Literal types - * note the : *
+let myName;
+let username;
+username = "jack";
+// Functions
+const add = (a, b) => {
+    return (a + b).toString();
+};
+const logMsg = (message) => {
+    console.log(message);
+};
+logMsg(vanHalen);
+function subtract(c, d) {
+    return c - d;
+}
+console.log(subtract(3, 4));
+let multiply = (c, d) => {
+    return c * d;
+};
+console.log(multiply(2, 5));
+// Optional parameters
+/* optional parameters need to be last */
+function addAll(a, b, c) {
+    if (typeof c !== "undefined") {
+        return a + b + c;
+    }
+    return a + b;
+}
+/* default parameter */
+function sumAll(a = 2, b, c = 2) {
+    return a + b + c;
+}
+console.log(addAll(2, 7, 0));
+console.log(addAll(2, 7, 5));
+console.log(sumAll(3, 7));
+console.log(sumAll(undefined, 7)); /* if we want to omit a parameter to use default we need to explicitly set it as undfined */
+// Rest parameters
+/* remember rest creates an arr of parameters */
+function total(num1 = 2, ...nums) {
+    return num1 + nums.reduce((prev, curr) => {
+        return prev + curr;
+    });
+}
+console.log(total(undefined, 2, 3, 4, 5));
+/* never type */
+/* never is used for errors */
+const createError = (error) => {
+    throw new Error(error);
+};
+/* never also alerts of infinite loop if cunction return never */
+const infiniteLoop = () => {
+    let i = 0;
+    while (true) {
+        i++;
+        if (i > 100)
+            break;
+    }
+};
+/* custom type guard */
+const isNumber = (value) => {
+    return (typeof value === "number") ? true : false;
+};
+/* uses for never */
+const numberOrString = (value) => {
+    if (typeof value === "string")
+        return "string";
+    if (isNumber(value))
+        return "number";
+    return createError("this should never happen");
+};
+console.log(numberOrString(""));
+/* 141 */ 
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,137 @@
 
-// Enum
-enum Grades {
-    a,
-    b,
-    c,
+/* Enums */
+
+/* unlike most typescript features, Enums are not a type-level addition to Javascript but something added to the language and the run time */
+
+enum Grade {
+    u = 1,
     d,
-    e
+    c,
+    b,
+    a,
 }
+
+console.log(Grade.a)
 
 // type aliases
-interface Guitarist {
+
+
+type stringOrNumber = ( string | number );
+
+type StringOrNumberArray = ( string | number )[];
+
+type Guitarist = {
     name?: string,
     active: boolean,
-    albums: (string | number )[],
+    albums: StringOrNumberArray,
 }
 
+let vanHalen: Guitarist = {
+    active: false,
+    name: "eddie",
+    albums: [1,"hi"],
+}
+
+// Literal types - * note the : *
+let myName: "tito"
+let username: "tito" | "jhon" | "jack";
+username = "jack"
+
+
+
+// Functions
+
+
+const add = ( a: number, b: number ):string => {
+    return (a + b).toString()
+}
+
+
+const logMsg = (message: any) => {
+    console.log(message)
+}
+
+logMsg(vanHalen);
+
+
+function subtract(c: number, d:number ) {
+    return c - d
+}
+
+console.log(subtract(3,4))
+
+
+// Function types
+type MathFunction = (a: number, b: number ) => number;
+
+// Function interface
+interface MathFunction2 {
+    (a: number, b: number ): number
+}
+
+let multiply: MathFunction = (c, d) => {
+    return c * d;
+}
+
+console.log(multiply(2, 5));
+
+// Optional parameters
+
+/* optional parameters need to be last */
+function addAll(a: number, b: number, c?: number): number {
+    if ( typeof c !== "undefined" ) {
+        return a + b + c
+    } 
+        return a + b
+}
+
+/* default parameter */
+function sumAll(a: number = 2, b: number, c: number = 2): number {
+        return a + b + c
+}
+
+console.log(addAll(2,7,0))
+console.log(addAll(2,7,5))
+console.log(sumAll(3,7))
+console.log(sumAll(undefined, 7)) /* if we want to omit a parameter to use default we need to explicitly set it as undfined */
+
+
+// Rest parameters
+/* remember rest creates an arr of parameters */
+function total(num1:number = 2, ...nums: number[]) {
+    return num1 + nums.reduce((prev, curr) => {
+        return prev + curr
+    })
+}
+
+console.log(total(undefined, 2,3,4,5))
+
+/* never type */
+/* never is used for errors */
+const createError = (error: string) => {
+    throw new Error(error)
+}
+
+/* never also alerts of infinite loop if cunction return never */
+const infiniteLoop = () => {
+    let i: number = 0;
+    while (true) {
+        i++
+        if (i > 100 ) break
+    }
+}
+/* custom type guard */
+const isNumber = (value: any): boolean => {
+    return (typeof value === "number") ? true : false;
+}
+
+/* uses for never */
+const numberOrString = (value: number | string): string => {
+    if (typeof value === "string") return "string";
+    if (isNumber(value)) return "number";
+    return createError("this should never happen")
+}
+
+console.log(numberOrString(""))
+
+/* 141 */
