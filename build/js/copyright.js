7cd18b01be749b73522c2cc640244c9dd3751598
Eduardo O <viverlin7@gmail.com>
2023-05-03 16:19:39 -0600

    Classes

--- build/js/copyright.js
+++ build/js/copyright.js
@@ -1,27 +1,82 @@
 "use strict";
-/* Vanilla js
+/* first example :
 
-const year = document.getElementById("year");
-const thisYear = new Date().getFullYear();
-year.setAttribute("datetime", thisYear);
-year.textContent = thisYear;
+class Coder {
+    name: string
+    music: string
+    age: number
+    lang: string
 
-*/
-// 1st variation 
-const year = document.getElementById("year");
-const thisYear = new Date().getFullYear().toString();
-year.setAttribute("datetime", thisYear);
-year.textContent = thisYear;
-//
-/* 2nd variation
-
-let year: HTMLElement | null;
-year = document.getElementById("year");
-let thisYear: string;
-thisYear = new Date().getFullYear().toString();
-if (year) {
-    year.setAttribute("datetime", thisYear);
-    year.textContent = thisYear;
+    constructor( name: string, music: string, age: number, lang: string ) {
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+    }
 }
 
 */
+/* second example */
+class Coder {
+    constructor(name, music, age, lang = "Typescript") {
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+    }
+    getAge() {
+        return `Hi! I'm ${this.age} years old`;
+    }
+}
+const Tito = new Coder("Tito", "Chill", 29);
+console.log(Tito.getAge());
+//console.log(Tito.age)
+//console.log(Tito.lang)
+class WebDev extends Coder {
+    constructor(computer, name, music, age) {
+        super(name, music, age);
+        this.computer = computer;
+        this.computer = computer;
+    }
+    getLang() {
+        return `I write ${this.lang}`;
+    }
+}
+const Kida = new WebDev("mac", "kida", "rock", 18);
+console.log(Kida.getLang());
+class Guitarist {
+    constructor(name, instrument) {
+        this.name = name;
+        this.instrument = instrument;
+        this.name = name;
+        this.instrument = instrument;
+    }
+    play(action) {
+        return `${this.name} ${action} the ${this.instrument}`;
+    }
+}
+const Page = new Guitarist("Jimmy", "Guitar");
+console.log(Page.play("strums"));
+/* Static member */
+/*
+Static means it does not apply to any instantiation of the class, it applies to the class directly
+*/
+class Peeps {
+    static getCount() {
+        return Peeps.count;
+    }
+    constructor(name) {
+        this.name = name;
+        this.name = name;
+        this.id = ++Peeps.count;
+    }
+}
+Peeps.count = 0;
+const Jack = new Peeps("Jack");
+const Steve = new Peeps("Steve");
+console.log(Peeps.count);
+console.log(Jack.id);
--- src/copyright.ts
+++ src/copyright.ts
@@ -1,33 +1,123 @@
-/* Vanilla js
-
-const year = document.getElementById("year");
-const thisYear = new Date().getFullYear();
-year.setAttribute("datetime", thisYear);
-year.textContent = thisYear; 
-
-*/
 
 
+/* first example :
 
-// 1st variation 
+class Coder {
+    name: string
+    music: string
+    age: number
+    lang: string
 
-const year = document.getElementById("year") as HTMLSpanElement;
-const thisYear: string = new Date().getFullYear().toString();
-year.setAttribute("datetime", thisYear);
-year.textContent = thisYear;
-
-//
-
-
-/* 2nd variation
-
-let year: HTMLElement | null;
-year = document.getElementById("year");
-let thisYear: string;
-thisYear = new Date().getFullYear().toString();
-if (year) {
-    year.setAttribute("datetime", thisYear);
-    year.textContent = thisYear;
+    constructor( name: string, music: string, age: number, lang: string ) {
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+    }
 }
 
 */
+
+/* second example */
+
+class Coder {
+
+    constructor(
+        readonly name: string,
+        public music: string,
+        private age: number,
+        protected lang: string = "Typescript"
+    ) {
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+    }
+
+    public getAge() {
+        return `Hi! I'm ${this.age} years old`
+    }
+}
+
+const Tito = new Coder("Tito", "Chill", 29,)
+console.log(Tito.getAge());
+//console.log(Tito.age)
+//console.log(Tito.lang)
+
+class WebDev extends Coder {
+
+    constructor(
+        public computer: string,
+        name: string,
+        music: string, 
+        age: number) {
+            super(name, music, age);
+            this.computer = computer
+    }
+
+    public getLang() {
+        return `I write ${this.lang}`
+    }
+}
+
+const Kida = new WebDev("mac", "kida", "rock", 18);
+console.log(Kida.getLang())
+//console.log(Kida.age)
+//console.log(Kida.lang)
+
+
+
+/* Class Interface */
+
+
+interface Musician {
+    name: string;
+    instrument: string;
+    play(action: string): string;
+}
+
+class Guitarist implements Musician {
+    
+    constructor(public name: string, public instrument: string) {
+        this.name = name;
+        this.instrument = instrument;
+    }
+
+    play(action: string) {
+        return `${this.name} ${action} the ${this.instrument}`
+    }
+}
+
+const Page = new Guitarist("Jimmy", "Guitar");
+console.log( Page.play("strums"))
+
+
+/* Static member */
+
+
+
+
+/*
+Static means it does not apply to any instantiation of the class, it applies to the class directly
+*/
+class Peeps {
+    static count: number = 0;
+
+    static getCount(): number {
+        return Peeps.count
+    }
+
+    public id: number;
+
+    constructor( public name: string) {
+        this.name = name;
+        this.id = ++Peeps.count;
+    }
+}
+
+const Jack = new Peeps("Jack");
+const Steve = new Peeps("Steve");
+
+
+console.log(Peeps.count)
+console.log(Jack.id)
